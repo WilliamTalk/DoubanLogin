01129be214b327b2d179e67db0c17824055e31aa
WilliamTalk <39369376+WilliamTalk@users.noreply.github.com>
2018-10-05 21:27:01 +0800

    if there no captcha_image ，postdat do have captcha-solution and captcha-id as well

--- Login/spiders/logindouban.py
+++ Login/spiders/logindouban.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import requests
-import  urllib.request
 from PIL import Image
 
 
@@ -11,38 +10,45 @@ class LogindoubanSpider(scrapy.Spider):
     start_urls = ['https://accounts.douban.com/login']
 
     def start_requests(self):
-        url='https://accounts.douban.com/login'
-     
-        yield  scrapy.Request(url=url,callback=self.parse)
+        url = 'https://accounts.douban.com/login'
 
+        return [scrapy.Request(url=url, callback=self.parse)]
 
-    def after_login(self,response):
-        title=response.css('head > title').extract_first()
-        print('title*****',title)
-
+    def after_login(self, response):
+        title = response.css('head > title').extract_first()
+        print('title*****', title)
 
     def parse(self, response):
-        imag=response.css('#captcha_image::attr(src)')
-        imagurl=imag.extract_first()
-        res=requests.get(imagurl)
-        with open("yanzhengma.jpg",'wb') as f:
-            f.write(res.content)
-        im = Image.open('yanzhengma.jpg')
-        im.show()
-        
-        captcha_id=imag.re('id=(.*?)&')[0]#.split('?')[1].split('&')[0]
-        captcha_solution=input()
         url = 'https://accounts.douban.com/login'
-        
-        postdata = {
-            'source': 'None',
-            'redir': 'https://www.douban.com/people/71210780/',
-            'form_email': '******@qq.com',
-            'form_password': '*********',
-            'captcha-solution': captcha_solution,
-            'captcha-id': captcha_id,
-            'login': '登录',
-        }
-        yield scrapy.FormRequest(url=url,formdata=postdata,callback=self.after_login,dont_filter=True)
+        imag = response.css('#captcha_image')
+        if imag is None:
+            imagurl = imag.extract_first()
+            res = requests.get(imagurl)
+            with open("yanzhengma.jpg", 'wb') as f:
+                f.write(res.content)
+            im = Image.open('yanzhengma.jpg')
+            im.show()
+
+            captcha_id = imag.re('id=(.*?)&')[0]  # .split('?')[1].split('&')[0]
+            captcha_solution = input()
 
 
+            postdata = {
+                'source': 'None',
+                'redir': 'https://www.douban.com/people/71210780/',
+                'form_email': '992621968@qq.com',
+                'form_password': 'william072424',
+                'captcha-solution': captcha_solution,
+                'captcha-id': captcha_id,
+                'login': '登录',
+            }
+        else:
+            postdata = {
+                'source': 'None',
+                'redir': 'https://www.douban.com/people/71210780/',
+                'form_email': '*****@qq.com',
+                'form_password': '*****',
+                'login': '登录',
+            }
+
+        yield scrapy.FormRequest(url=url, formdata=postdata, callback=self.after_login, dont_filter=True)
